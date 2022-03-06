eb81eda041faa95f12edb753d33614e941f1dded
Patgio <Patgiok@gmail.com>
2022-03-06 14:19:57 -0800

    comment whitespace legibility

--- src/index.tsx
+++ src/index.tsx
@@ -50,10 +50,10 @@ ReactDOM.render(
                             }
                         />
                         {/* index route, 
-            Index routes render in the parent routes outlet at the parent route's path.
-            Index routes match when a parent route matches but none of the other children match.
-            Index routes are the default child route for a parent route.
-            Index routes render when the user hasn't clicked one of the items in a navigation list yet. */}
+                         Index routes render in the parent routes outlet at the parent route's path.
+                         Index routes match when a parent route matches but none of the other children match.
+                         Index routes are the default child route for a parent route.
+                          Index routes render when the user hasn't clicked one of the items in a navigation list yet. */}
                         <Route path=":invoiceId" element={<Invoice />} />
                     </Route>
 
